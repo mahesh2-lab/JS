0fc16d8ebcb03256aa77518a0611529e2ecd5ceb
Mahesh <maheshchopade133@gmail.com>
2025-01-03 18:03:09 +0530

    calculator

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
 
-git config --global user.email "you@example.com"
-'git config --global user.name "Your Name"'
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
+
+console.log("Addit  ion: " + add(5, 3));
+console.log("Subtraction: " + subtract(5, 3));
+console.log("Multiplication: " + multiply(5, 3));
+console.log("Division: " + divide(5, 3));
